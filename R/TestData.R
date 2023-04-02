ba570db197556c9744031454ebc611f87674baa1
kan Lei <kan.lei@tu-dresden.de>
2023-04-02 22:46:04 +0200

    example with model's run

---
+++ R/TestData.R
@@ -0,0 +1,7 @@
+#' Test data for modelling
+#' @name test_data
+#' @description Precipitation, temperature and potential evapotranspiration
+#' Test data contain three data precipitation (7 forms, start with `P`), temperature (5 forms, start with `T`) 
+#' and potential evapotranspiration (2 forms, start with `E`), 
+#' each with 180 time steps. They can be used to test the model's run.
+"EDCHM_TestData"
